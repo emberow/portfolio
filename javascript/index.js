feb6ee94a8735deb695c343131b3b8a40b109eba
emberow <80031876+emberow@users.noreply.github.com>
2022-03-06 23:48:54 +0800

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -20,3 +20,25 @@ $(document).ready(function(){
 function back_to_top(){
     window.location = "#";
 }
+
+function show_article1(){
+    $('#article1').css({
+        "display":'block'
+    });
+}
+
+function show_article2(){
+    $('#article2').css({
+        "display":'block'
+    });
+}
+
+function esc_button(){
+    $('#article1').css({
+        "display":'none'
+    });
+    $('#article2').css({
+        "display":'none'
+    });
+    
+}
